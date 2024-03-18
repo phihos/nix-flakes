4d4525f6f1f003d9e3a2e884fff0ba495df53c77
Philipp Hossner <philipp.hossner@posteo.de>
2024-03-19 00:51:16 +0100

    40 current  2024-03-19 00:51:13  24.05.20240316.c75037b   6.6.22                          *

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-software-center.url = "github:snowfallorg/nix-software-center";
+
+    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
+    alejandra.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations.der-geraet = nixpkgs.lib.nixosSystem {
+      specialArgs = {
+        inherit inputs;
+      };
+      modules = [
+        ./hosts/der-geraet/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
---
+++ scripts/apply.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+pushd "${SCRIPT_DIR}/.."
+alejandra . &>/dev/null
+sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+gen=$(nixos-rebuild list-generations | grep current)
+git commit -am "$gen"
+popd
