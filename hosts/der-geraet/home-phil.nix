56962858037e1a0b45a1847bab302a28e80b39a7
Philipp Hossner <philipp.hossner@posteo.de>
2024-03-19 17:38:15 +0100

    49 current  2024-03-19 17:38:12  24.05.20240316.c75037b   6.6.22                          *

--- hosts/der-geraet/home-phil.nix
+++ hosts/der-geraet/home-phil.nix
@@ -4,6 +4,7 @@
   ...
 }: {
   imports = [
+    ./home-common.nix
     ./home-phil-secret.nix
   ];
 
@@ -11,15 +12,6 @@
   # manage.
   home.username = "phil";
   home.homeDirectory = "/home/phil";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -74,17 +66,5 @@
   #
   home.sessionVariables = {
     # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-    "org/gnome/desktop/session" = {
-      idle-delay = 0;
-    };
   };
 }
--- hosts/der-geraet/home-philipp-privat.nix
+++ hosts/der-geraet/home-philipp-privat.nix
@@ -4,21 +4,13 @@
   ...
 }: {
   imports = [
+    ./home-common.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "philipp-privat";
   home.homeDirectory = "/home/philipp-privat";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -73,17 +65,5 @@
   #
   home.sessionVariables = {
     # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-    "org/gnome/desktop/session" = {
-      idle-delay = 0;
-    };
   };
 }
