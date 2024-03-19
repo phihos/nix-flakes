f9e42bc13ec2964c11314632baa329407844735d
Philipp Hossner <philipp.hossner@posteo.de>
2024-03-19 12:22:56 +0100

    46 current  2024-03-19 12:22:53  24.05.20240316.c75037b   6.6.22                          *

--- hosts/der-geraet/packages.nix
+++ hosts/der-geraet/packages.nix
@@ -4,74 +4,64 @@
   inputs,
   ...
 }: {
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   # Search "programs.<name>" https://search.nixos.org/options before.
   environment.systemPackages = with pkgs; [
-    pkgs.yubioath-flutter
-    inputs.nix-software-center.packages.${system}.nix-software-center
-    vim
-    wget
-    pciutils
-    jetbrains.pycharm-professional
-    jetbrains.idea-ultimate
-    jetbrains.goland
-    jetbrains.ruby-mine
-    ruby
-    rubyPackages.ffi
-    kubectl
-    vagrant
-    libvirt
-    virt-manager
-    krew
-    kubetail
-    slack
     git-crypt
+    gnome.gnome-tweaks
+    gnome.networkmanager-openvpn
     gnomeExtensions.appindicator
     gnomeExtensions.caffeine
     gnomeExtensions.clipboard-indicator
-    gnomeExtensions.utcclock
-    networkmanager-openvpn
-    gnome.networkmanager-openvpn
     gnomeExtensions.dash-to-dock
-    zoom-us
-    gnome.gnome-tweaks
+    gnomeExtensions.utcclock
+    inputs.nix-software-center.packages.${system}.nix-software-center
+    jetbrains.goland
+    jetbrains.idea-ultimate
+    jetbrains.pycharm-professional
+    jetbrains.ruby-mine
+    krew
+    kubectl
+    kubetail
+    libvirt
+    networkmanager-openvpn
+    pciutils
     pdk
+    ruby
+    rubyPackages.ffi
+    slack
+    vagrant
+    vim
+    virt-manager
+    wget
+    yubioath-flutter
+    zoom-us
     inputs.alejandra.defaultPackage.${system}
     (vscode-with-extensions.override {
       vscodeExtensions = with vscode-extensions; [
         bbenoist.nix
-        ms-python.python
-        ms-azuretools.vscode-docker
-        ms-vscode-remote.remote-ssh
         kamadorueda.alejandra
+        ms-azuretools.vscode-docker
+        ms-python.python
+        ms-vscode-remote.remote-ssh
       ];
     })
   ];
 
-  programs.wireshark.enable = true;
-  programs.virt-manager.enable = true;
+  programs._1password-gui.enable = true;
+  programs._1password-gui.polkitPolicyOwners = ["phil"];
+  programs._1password.enable = true;
   programs.firefox.enable = true;
-  programs.yubikey-touch-detector.enable = true;
   programs.git.enable = true;
   programs.git.lfs.enable = true;
-  virtualisation.docker.enable = true;
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    # Certain features, including CLI integration and system authentication support,
-    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-    polkitPolicyOwners = ["phil"];
-  };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
+  programs.gnupg.agent.enable = true;
+  programs.gnupg.agent.enableSSHSupport = true;
   programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
+  programs.virt-manager.enable = true;
+  programs.wireshark.enable = true;
+  programs.yubikey-touch-detector.enable = true;
+  virtualisation.docker.enable = true;
 }
