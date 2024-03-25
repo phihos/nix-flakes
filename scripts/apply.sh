f2b8dc9a25647e668f05d7a27833a87af7d33ec7
Philipp Hossner <philipp.hossner@posteo.de>
2024-03-25 09:32:21 +0100

    59 current  2024-03-25 09:32:18  24.05.20240316.c75037b   6.6.22                          *

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -4,7 +4,7 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd "${SCRIPT_DIR}/.."
 alejandra . &>/dev/null
-sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+sudo nixos-rebuild switch --flake . |& nom
 gen=$(nixos-rebuild list-generations | grep current)
 git commit -am "$gen"
 popd
