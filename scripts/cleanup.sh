0cc81e2b3c8ffbf0a11533d1d25c9d385bb6dc29
Philipp Hossner <philipp.hossner@posteo.de>
2024-04-05 20:07:17 +0200

    system-upgrade

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,4 +2,8 @@
 set -e
 
 sudo nix-collect-garbage --delete-older-than '7d'
+echo ""
+echo "Remaining generations:"
+nix-env --list-generations
+echo ""
 df -h | grep -v /run | grep -v /sys | grep -v '% /dev'
