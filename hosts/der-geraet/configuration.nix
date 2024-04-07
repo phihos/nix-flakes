438fdcf38e843c5b3f43f8636c8b8260423ab910
Philipp Hossner <philipp.hossner@posteo.de>
2024-04-07 02:15:02 +0200

    128 current  2024-04-07 02:14:58  24.05.20240403.fd281bd  6.6.24                          *

--- hosts/der-geraet/configuration.nix
+++ hosts/der-geraet/configuration.nix
@@ -33,10 +33,9 @@
 
   services.resolved = {
     enable = true;
-    dnssec = "true";
-    domains = ["~."];
+    dnssec = "allow-downgrade";
     fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
-    dnsovertls = "true";
+    dnsovertls = "opportunistic";
   };
 
   # Set your time zone.
