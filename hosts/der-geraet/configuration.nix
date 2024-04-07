c410eaaf932c09498c4e53b0499aa8df6a3f888a
Philipp Hossner <philipp.hossner@posteo.de>
2024-04-07 02:00:24 +0200

    126 current  2024-04-07 02:00:13  24.05.20240403.fd281bd  6.6.24                          *

--- hosts/der-geraet/configuration.nix
+++ hosts/der-geraet/configuration.nix
@@ -29,6 +29,15 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  networking.nameservers = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = ["~."];
+    fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+    dnsovertls = "true";
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
