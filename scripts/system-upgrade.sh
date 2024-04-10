e10e2b10a3f4dd1986b2032d16f0c84c85f4219c
Philipp Hossner <philipp.hossner@posteo.de>
2024-04-10 10:18:15 +0200

    system-upgrade

--- scripts/system-upgrade.sh
+++ scripts/system-upgrade.sh
@@ -3,6 +3,6 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd "${SCRIPT_DIR}/.."
-sudo nixos-rebuild switch --flake . --upgrade --update-input nixpkgs
+sudo nixos-rebuild switch --flake . --upgrade --upgrade-all
 git commit -am "system-upgrade"
 popd
