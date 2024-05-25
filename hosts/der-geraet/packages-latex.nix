ffc58b9eab9940facc00ddc2fe4f010cf19dee88
Philipp Hossner <philipp.hossner@posteo.de>
2024-05-25 13:53:28 +0200

    235 current  2024-05-25 13:53:15  24.05.20240521.5710852  6.6.31                          *

--- hosts/der-geraet/packages-latex.nix
+++ hosts/der-geraet/packages-latex.nix
@@ -4,12 +4,9 @@
   ...
 }: let
   my-latex-packages = with pkgs;
-    texlive.withPackages (ps:
+    texliveFull.withPackages (ps:
       with ps; [
-        texdoc # recommended package to navigate the documentation
-        perlPackages.LaTeXML.tex # tex files of LaTeXML, omit binaries
-        cm-super
-        cm-super.texdoc # documentation of cm-super
+        texlivePackages.minted
       ]);
 in {
   environment.systemPackages = [
