446881c6099b4db11015cf201fde2ac21664dc28
Philipp Hossner <philipp.hossner@posteo.de>
2024-06-16 12:56:13 +0200

    system-upgrade

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
