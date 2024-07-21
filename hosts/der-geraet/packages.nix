4fb0bd68cac02c5565290a52c9383c2b60ad4bac
Philipp Hossner <philipp.hossner@posteo.de>
2024-07-21 22:55:29 +0200

    WIP

--- hosts/der-geraet/packages.nix
+++ hosts/der-geraet/packages.nix
@@ -192,6 +192,7 @@
   };
   virtualisation.libvirtd.enable = true;
   virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
   users.extraGroups.vboxusers.members = [
     "phil"
     "philipp-privat"
